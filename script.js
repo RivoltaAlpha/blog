a2e437d1db8355793314d88ed10d6e9a6cc17683
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-05-29 21:28:22 +0300

    mobile view

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// function subscribe() {
-//   const emailInput = document.getElementById("email");
-//   const email = emailInput.value;
-
-//   if (email) {
-//     document.getElementById("subscriber-email").innerText = email;
-//     document.getElementById("overlay").style.display = "flex";
-//   } else {
-//     alert("Please enter a valid email address.");
-//   }
-// }
-
-// function dismissMessage() {
-//   document.getElementById("overlay").style.display = "block";
-//   document.getElementById("thankyou-card").style.display = "block";
-//   document.getElementById("email").value = "";
-// }
 // JavaScript to open and close the modal
 var modal = document.getElementById("thankyou-card");
 var btn = document.getElementById("subscribe");
