cd2c165540d4d5ba1a0e4c426063532c9ee6eea7
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-06-01 13:15:15 +0300

    blur background

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-// JavaScript to open and close the modal
-var modal = document.getElementById("thankyou-card");
-var btn = document.getElementById("subscribe");
-var closeButton = document.getElementsByClassName("close")[0];
-var mainContent = document.getElementById("mainContent");
-// var emailContent = getElementById("email");
+document.addEventListener("DOMContentLoaded", () => {
+  // JavaScript to open and close the modal
+  var modal = document.getElementById("thankyou-card");
+  var btn = document.getElementById("subscribe");
+  var closeButton = document.getElementsByClassName("close")[0];
+  var mainContent = document.getElementById("mainContent");
+  var bodyContainer = document.getElementById("container");
+  const forms = document.forms;
+  const subscribeForm = forms["subscriptionForm"];
 
-btn.onclick = function () {
-  modal.style.display = "block";
-  mainContent.classList.add("blur-background");
-};
+  btn.onclick = function () {
+    modal.style.display = "block";
+    mainContent.classList.add("blur-background");
+    bodyContainer.classList.add("blur-background");
+    //clear inputs
+    subscribeForm.querySelector('input[type="email"]').value = "";
+  };
 
-closeButton.onclick = function () {
-  modal.style.display = "none";
-  mainContent.classList.remove("blur-background");
-  // document.getElementById("email").value = "";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
+  closeButton.onclick = function () {
     modal.style.display = "none";
     mainContent.classList.remove("blur-background");
-  }
-};
+    bodyContainer.classList.remove("blur-background");
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      mainContent.classList.remove("blur-background");
+    }
+  };
+});
